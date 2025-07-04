c5ff0896dee82d379a014863cc5b5fefeccd766e
Nishant kumar <nishant201kashyap@gmail.com>
2025-07-04 17:50:18 +0530

    update

--- Index.js
+++ Index.js
@@ -2,12 +2,21 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.listen(port,()=>{
-    console.log(`The poprt ${port} is being initialised`);
-})
+app.set("view engine","ejs");
+
+
+
 
 app.get("/",(req,res)=>{
-    res.send("Hi how can I help you");
+    res.send("Welcome");
     console.log("The request achieved");
 })
 
+
+app.get("/Home",(req,res)=>{
+    res.render("Home.ejs");    
+})
+
+app.listen(port,()=>{
+    console.log(`The port ${port} is being initialised`);
+})
