bff464085471347a23d2114a46c6226875c1ea05
Nishant kumar <nishant201kashyap@gmail.com>
2025-07-04 19:17:51 +0530

    update

--- Index.js
+++ Index.js
@@ -2,15 +2,15 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.set("view engine","ejs");
-
+// const path = require("path");
+// app.set("views",path.join(EJS-Template,"/views"));
 
 app.get("/",(req,res)=>{
     res.send("Welcome");
     console.log("The request achieved");
 })
 
-
+app.set("view engine","ejs");
 app.get("/Home",(req,res)=>{
     res.render("Home.ejs");    
     console.log("Request achieved");
