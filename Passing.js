f9019fd87657fdf1db779decca60d332fd63d20f
Nishant kumar <nishant201kashyap@gmail.com>
2025-07-05 23:47:57 +0530

    update

---
+++ Passing.js
@@ -0,0 +1,21 @@
+const express  = require("express");
+const app = express();
+const port = 8080;
+
+app.listen(port,()=>{
+    console.log(`The request from client to port ${port}`);
+})
+
+app.set("views engine","ejs");
+
+
+app.get("/rolldice",(req,res)=>{
+    let num = Math.floor(Math.random()*6+1);
+    console.log("The output provided");
+    res.render("rolldice.ejs",{diceVal: num});
+})
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to home page");
+    console.log("respinse provided");
+})
