e932c08e1859fa37268a178009c8f133f458978a
Nishant kumar <nishant201kashyap@gmail.com>
2025-07-06 14:47:51 +0530

    add

---
+++ Instagram.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+
+app.set("view engine","ejs");
+
+app.listen(port,()=>{
+    console.log(`The port ${port} is initialised`);
+});
+
+app.get("/",(req,res)=>{
+    res.send("Welcome to the Instagram");
+    console.log("The request acheived");
+})
+
+app.get("/instagram/:username",(req,res)=>{
+    let {username}=req.params;
+    res.render("Instagram.ejs",{username});
+    console.log(`Entered into ${username} instagram ID`);
+})
